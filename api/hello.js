9734cf1b8b2a354efa7152083f0d69f7bdb090b3
Raman Dhawan <8374679+raevilman@users.noreply.github.com>
2021-09-17 15:52:38 +0530

    debug vlog

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,8 @@
 module.exports = (req, res) => {
-    res.json({
-      msg: 'hello there, how are you!!'
-    });
-  };
+  let msg = "hello there, how are you!!";
+
+  // msg = "Boo!";
+  res.json({
+    msg,
+  });
+};
